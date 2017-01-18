0af454ceef9ac6ebaffa521a3b2afd7be5f21002
LeeBlu <pseshibe@gmail.com>
2017-01-18 08:38:50 +0200

    modified project

--- E-Learning/Models/ViewModel.cs
+++ E-Learning/Models/ViewModel.cs
@@ -10,6 +10,8 @@ namespace E_Learning.Models
         public IEnumerable<Subject> subject { get; set; }
         public IEnumerable<Mentor> mentor { get; set; }
         public IEnumerable<Learner> learner { get; set; }
+        public IEnumerable<Problem> problem { get; set; }
+        public IEnumerable<Solution> solution { get; set; }
 
     }
 }
